9092aef088aaca4bfac164b0750a881dc8d1340f
CHIp Wright <frisbeeprod@msn.com>
2016-11-20 23:54:07 -0700

    More files

--- projects/poke-profile/index.js
+++ projects/poke-profile/index.js
@@ -1,6 +1,6 @@
 var app = angular.module("PokeApp", []);
 
-app.controller("MainCtrl", ["$scope", "pokemonService", function($scope, $pokemonService) {
+app.controller("MainCtrl", ["$scope", "pokemonService", function($scope, pokemonService) {
     
     $scope.getPokemon = function(pokemonNumber) {
         pokemonService.getPokemonInfo(pokemonNumber)
@@ -15,16 +15,21 @@ app.service("pokemonService", ["$http", function($http) {
     this.getPokemonInfo = function(pokemonNumber) {
         var baseUrl = "http://pokeapi.co";
         var pokemon = {};
-        return $http.get(baseUrl + "http://pokeapi.co/api/v1/pokemon/" + pokemonNumber)
+        
+        return $http.get(baseUrl + "/api/v1/pokemon/" + pokemonNumber)
             .then(function (response) {
                 pokemon.name = response.data.name;
+                    console.log(pokemon.name)
                 pokemon.attack = response.data.attack;
+                    console.log(pokemon.attack)
                 pokemon.ability = response.data.abilities[0].name;
-                return $http.get(baseUrl + response.data.sprites[0].resources_url);
-            }).then(function(response) {
-                pokemon.image = baseUrl + response.data.image;
-                return pokemon;
-            })
+                    console.log(pokemon.ability)
+//                return $http.get(baseUrl + response.data.sprites[0].resources_url);
+           })
+//                        .then(function(response) {
+//                pokemon.image = baseUrl + response.data.image;
+//                return pokemon;
+//            })
     }
 }]);
 

74a475d3b54a5bd89a03cb8e6c3aba9ad85db8c3
CHIp Wright <frisbeeprod@msn.com>
2016-11-30 07:56:29 -0700

    More files

--- projects/second-full-stack/backend/server.js
+++ projects/second-full-stack/backend/server.js
@@ -1,13 +1,14 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-var app = express();
 var port = 8000;
+var app = express();
+
 
 app.get("/", function(req, res) {
     res.send("Heyyooooo");
 })
-
+//run the server
 app.listen(port, function() {
     console.log("Server is up on port 8000");
 })
